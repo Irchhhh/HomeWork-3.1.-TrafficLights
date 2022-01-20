c73291bd264197bad4782138ad9bd9f071df17d2
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2022-01-20 22:55:49 +0300

    Initial Commit

---
+++ HomeWork 3.1. TrafficLights/HomeWork_3_1__TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_3_1__TrafficLightsApp.swift
+//  HomeWork 3.1. TrafficLights
+//
+//  Created by Ирина on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_3_1__TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
