5eca38c3ccd5d8435b10a8e6d9674af3accb528b
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2022-01-21 16:37:31 +0300

    first commit

---
+++ HomeWork 3.1. TrafficLights/ColorsView.swift
@@ -0,0 +1,90 @@
+//
+//  RedColorView.swift
+//  HomeWork 3.1. TrafficLights
+//
+//  Created by Ирина on 20.01.2022.
+//
+
+import SwiftUI
+
+//MARK: - Red Color
+struct RedColorView: View {
+    var opacity: Double
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            redColorView
+                .padding(.bottom, 350)
+        }
+    }
+    private var redColorView: some View {
+        VStack {
+            Circle()
+                .opacity(opacity)
+                .frame(width: 100, height: 100)
+                .foregroundColor(.red)
+                .overlay(Circle().stroke(.white, lineWidth: 3))
+        }
+    }
+}
+//MARK: - Yellow Color
+struct RedColorView_Previews: PreviewProvider {
+    static var previews: some View {
+        RedColorView(opacity: 0.3)
+    }
+}
+
+struct YellowColorView: View {
+    var opacity: Double
+    
+    var body: some View {
+        ZStack {
+            yellowColorView
+                .padding(.bottom, 130)
+        }
+    }
+    
+    private var yellowColorView: some View {
+        VStack {
+            Circle()
+                .opacity(opacity)
+                .foregroundColor(.yellow)
+                .frame(width: 100, height: 100)
+                .overlay(Circle().stroke(.white, lineWidth: 3))
+        }
+    }
+}
+
+struct YellowColorView_Previews: PreviewProvider {
+    static var previews: some View {
+        YellowColorView(opacity: 0.3)
+    }
+}
+
+//MARK: - Green Color
+struct GreenColorView: View {
+    var opacity: Double
+    
+    private var greenColorView: some View {
+        VStack {
+            Circle()
+                .foregroundColor(.green)
+                .opacity(opacity)
+                .frame(width: 100, height: 100)
+                .overlay(Circle().stroke(.white, lineWidth: 3))
+        }
+    }
+    var body: some View {
+        greenColorView
+            .padding(.bottom, -90)
+    }
+}
+
+struct GreenColorView_Previews: PreviewProvider {
+    static var previews: some View {
+        GreenColorView(opacity: 0.3)
+    }
+}
+
